26ef9a0bef7d3220e842d1825596d970b7963225
Thom <thomkok13@hotmail.com>
2018-05-07 09:00:13 +0200

    Update

--- layout/header.php
+++ layout/header.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <?php if ($pagina === 'beheerder') { ?>
-    <link rel="stylesheet" href="../css/beheerder.css">
+        <link rel="stylesheet" href="../css/beheerder.css">
     <?php } ?>
 </head>
 <body>
@@ -22,20 +22,25 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li <?php if ($pagina === 'attractie') { ?> class="active" <?php } ?> ><a href="index.php">Attracties</a></li>
+                <li <?php if ($pagina === 'attractie') { ?> class="active" <?php } ?> ><a
+                            href="index.php">Attracties</a></li>
                 <?php if (isset($_SESSION['login']) && $_SESSION['login']['rechten'] == 'Beheerder') { ?>
-                    <li <?php if ($pagina === 'beheerder') { ?> class="active" <?php } ?> ><a href="beheerder.php">Beheren</a></li>
+                    <li <?php if ($pagina === 'beheerder') { ?> class="active" <?php } ?> ><a href="beheerder.php">Beheren</a>
+                    </li>
                 <?php } ?>
                 <?php if (!isset($_SESSION['login'])) { ?>
-                    <li <?php if ($pagina === 'registreren') { ?> class="active" <?php } ?> ><a href="registreren.php">Registreren</a></li>
+                    <li <?php if ($pagina === 'registreren') { ?> class="active" <?php } ?> ><a href="registreren.php">Registreren</a>
+                    </li>
                 <?php } ?>
                 <?php if (isset($_SESSION['login'])) { ?>
                     <li><a href="loguit.php">Uitloggen</a></li>
-                    <li><img style="width:42px;height:42px; margin-top:4px;" src="img/<?php echo $_SESSION['login']['login']; ?>.png"
+                    <li><img style="width:42px;height:42px; margin-top:4px;"
+                             src="img/<?php echo $_SESSION['login']['login']; ?>.png"
                              alt="plaatje gebruiker"></li>
                     <li><a><?php echo $_SESSION['login']['volledige naam']; ?></a></li>
                 <?php } else { ?>
-                    <li <?php if ($pagina === 'login') { ?> class="active" <?php } ?>><a href="login.php">Inloggen</a></li>
+                    <li <?php if ($pagina === 'login') { ?> class="active" <?php } ?>><a href="login.php">Inloggen</a>
+                    </li>
                 <?php } ?>
             </ul>
 
